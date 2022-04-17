5bf3708b529d569b9e3487995565fa2c13e5285f
Michael Hansen <mm@lesmotet.com>
2022-04-17 19:07:51 +0800

    added eb hardcoded vars

--- djangoexample/settings.py
+++ djangoexample/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'h(vmzdhd$5xqzd$m*%cf5^6!gcsz)l=99^y^360h^o%@x5($2g' #env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
