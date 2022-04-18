88796b17ee9c73ebbd73015f6f383589b397b2fc
Michael Hansen <mm@lesmotet.com>
2022-04-18 12:04:06 +0800

    fixes on eb configs

--- djangoexample/settings.py
+++ djangoexample/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'djangoexample.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.db.backends.mysql',
             # 'NAME': os.environ['RDS_DB_NAME'],
             # 'USER': os.environ['RDS_USERNAME'],
             # 'PASSWORD': os.environ['RDS_PASSWORD'],
